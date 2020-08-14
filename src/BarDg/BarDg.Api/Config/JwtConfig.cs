2a188b5402b911691d60de33ba37fa926dd502a3
Felipe Dantas <felipe.simplicio.dantas@hotmail.com>
2020-08-14 16:16:00 -0300

    Commit inicial

---
+++ src/BarDg/BarDg.Api/Config/JwtConfig.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BarDg.Api.Config
+{
+    public static class JwtConfig
+    {
+        public static IServiceCollection JwtConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtChaveSecreta = configuration["JwtChaveSecreta"];
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtChaveSecreta)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseJwtConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+
+            return app;
+        }
+    }
+}
---
+++ src/BarDg/BarDg.Api/Config/SwaggerConfig.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BarDg.Api.Config
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection SwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "API - Bar do DG",
+                        Version = "1.0",
+                        Description = $"Projeto criado para teste.",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Felipe Dantas",
+                            Email = "felipe.simplicio.dantas@hotmail.com",
+                            Url = new Uri("https://github.com/felidan/")
+                        }
+                    });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization - Header de de autorização utilizando Bearer. Exemplo: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfigutation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bar do DG");
+                c.RoutePrefix = "doc";
+            });
+            return app;
+        }
+    }
+}
